1ba57487a5aad2b83f0e49e84ca4cbeadcf7feae
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2025-04-24 12:25:55 -0400

    chore: enable global validation with DTO transformation and strict property checks

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './infraestructure/config/configuration';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './infraestructure/config/swagger.config';
 import { AllExceptionsFilter } from './infraestructure/exception-filters/all-exceptions.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const { port } = config();
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document);
